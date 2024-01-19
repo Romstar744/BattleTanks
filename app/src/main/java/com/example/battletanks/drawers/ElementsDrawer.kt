6faa7007241a2efd0e14231338fafa950642ee47
Romstar744 <romstar742@gmail.com>
2024-01-19 21:25:07 +0300

    презентация №5

--- app/src/main/java/com/example/battletanks/drawers/ElementsDrawer.kt
+++ app/src/main/java/com/example/battletanks/drawers/ElementsDrawer.kt
@@ -3,14 +3,18 @@ package com.example.battletanks.drawers
 import android.view.View
 import android.widget.FrameLayout
 import android.widget.ImageView
+import androidx.core.view.marginLeft
+import androidx.core.view.marginTop
 import com.example.battletanks.CELL_SIZE
 import com.example.battletanks.R
+import com.example.battletanks.binding
+import com.example.battletanks.enums.Direction
 import com.example.battletanks.enums.Material
 import com.example.battletanks.models.Coordinate
 import com.example.battletanks.models.Element
 
 class ElementsDrawer(val container: FrameLayout) {
-var currentMaterial = Material.EMPTY
+    var currentMaterial = Material.EMPTY
     private val elementsOnContainer = mutableListOf<Element>()
 
     fun onTouchContainer(x: Float, y: Float) {
@@ -27,6 +31,7 @@ var currentMaterial = Material.EMPTY
             Material.EMPTY -> {
 
             }
+
             Material.BRICK -> view.setImageResource(R.drawable.brick)
             Material.CONCRETE -> view.setImageResource(R.drawable.concrete)
             Material.GRASS -> view.setImageResource(R.drawable.grass)
@@ -38,5 +43,76 @@ var currentMaterial = Material.EMPTY
         view.layoutParams = layoutParams
         container.addView(view)
         elementsOnContainer.add(Element(viewId, currentMaterial, coordinate))
+    }
+
+    fun move(myTank: View, direction: Direction) {
+        val layoutParams = myTank.layoutParams as FrameLayout.LayoutParams
+        val currentCoordinate = Coordinate(layoutParams.topMargin, layoutParams.leftMargin)
+        when (direction) {
+            Direction.UP -> {
+                myTank.rotation = 0f
+                (myTank.layoutParams as FrameLayout.LayoutParams).topMargin += -CELL_SIZE
+            }
+
+            Direction.DOWN -> {
+                myTank.rotation = 180f
+                (myTank.layoutParams as FrameLayout.LayoutParams).topMargin += CELL_SIZE
+            }
+
+            Direction.LEFT -> {
+                myTank.rotation = 270f
+                (myTank.layoutParams as FrameLayout.LayoutParams).leftMargin -= CELL_SIZE
+            }
+
+            Direction.RIGHT -> {
+                myTank.rotation = 90f
+                (myTank.layoutParams as FrameLayout.LayoutParams).leftMargin += CELL_SIZE
+            }
+        }
+
+        val nextCoordinate = Coordinate(layoutParams.topMargin, layoutParams.leftMargin)
+        if (checkTankCanMoveThroughBorder(
+                nextCoordinate,
+                myTank
+            ) && checkTankCanMoveThroughMaterial(nextCoordinate)
+        ) {
+            binding.container.removeView(binding.myTank)
+            binding.container.addView(binding.myTank)
+        } else {
+            (myTank.layoutParams as FrameLayout.LayoutParams).topMargin = currentCoordinate.top
+            (myTank.layoutParams as FrameLayout.LayoutParams).leftMargin = currentCoordinate.left
+        }
+    }
+
+    private fun getElementByCoordinates(coordinate: Coordinate) =
+        elementsOnContainer.firstOrNull{ it.coordinate == coordinate}
+
+    private fun checkTankCanMoveThroughMaterial(coordinate: Coordinate): Boolean {
+        getTankCoordinates(coordinate).forEach{
+            val element = getElementByCoordinates(it)
+            if (element != null && !element.material.tankConGoThrough) {
+                return false
+            }
+        }
+        return true
+    }
+    private fun checkTankCanMoveThroughBorder(coordinate: Coordinate, myTank: View): Boolean {
+        return coordinate.top >= 0 &&
+            coordinate.top + myTank.height <= binding.container.height &&
+            coordinate.left >= 0 &&
+            coordinate.left + myTank.width <= binding.container.width
+    }
+    private fun getTankCoordinates(topLeftCoordinate: Coordinate): List<Coordinate> {
+        val coordinateList = mutableListOf<Coordinate>()
+        coordinateList.add(topLeftCoordinate)
+        coordinateList.add(Coordinate(topLeftCoordinate.top + CELL_SIZE, topLeftCoordinate.left))
+        coordinateList.add(Coordinate(topLeftCoordinate.top, topLeftCoordinate.left + CELL_SIZE))
+        coordinateList.add(
+            Coordinate(
+                topLeftCoordinate.top + CELL_SIZE,
+                topLeftCoordinate.left + CELL_SIZE
+            )
+        )
+        return coordinateList
     }
 }
